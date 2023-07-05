cc7e20bc6ff7b5932fe2874089241fb3acb143af
Panos Bezas <p.bezas1@gmail.com>
2023-07-05 11:59:00 +0300

    first commit

---
+++ Tenzies_Game/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import Box from "@mui/material/Box";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Box className="dice-container" sx={{  }}>
+        {" "}
+      </Box>
+    </div>
+  );
+}
